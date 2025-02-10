11623a7e6c0a41bb870afe0ca847b118b61da37b
Mateo <mateobarac0712@gmx.de>
2025-02-10 16:10:02 +0100

    QuickSort implementiert (Copilot Hilfe)

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -2,15 +2,33 @@ package quicksort
 
 // QuickSort sortiert die gegebene Liste mit dem Quicksort-Algorithmus.
 func QuickSort(list []int) {
+
+	//Abbruchbedingung
 	if len(list) < 2 {
 		return
 	}
 
-	//Pivot Element wählen
-	//Liste durchlaufen und Elemente mit Pivotelement vergleichen
-	//Kleinere und Größere Elemente in seperaten Listen trennen
+	//Der Pivot ist das Letzte Element
+	pivotIndex := len(list) - 1
+	pivot := list[pivotIndex]
 
-	//Links und Rechts sortieren
+	//Counter i startet bei 0
+	i := 0
 
-	//Listen wieder zusammenkopieren
+	//j ranged
+	for j := range list {
+
+		//Wenn list[j] kleiner als der Pivot ist
+		if list[j] < pivot {
+			//Element bei i mit Element bei j vertauschen
+			list[i], list[j] = list[j], list[i]
+			i++
+		}
+	}
+
+	//Pivotelement mit i tauschen
+	list[i], list[pivotIndex] = list[pivotIndex], list[i]
+
+	QuickSort(list[:i])
+	QuickSort(list[i+1:])
 }
